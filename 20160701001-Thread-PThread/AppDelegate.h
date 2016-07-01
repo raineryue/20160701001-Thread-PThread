8548dbdcdb3ed10eb5e53f1a4757a39f8e808555
Rainer <it_jinlei.yue@163.com>
2016-07-01 13:55:43 +0800

    Initial Commit

---
+++ 20160701001-Thread-PThread/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  20160701001-Thread-PThread
+//
+//  Created by Rainer on 16/7/1.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 20160701001-Thread-PThread/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  20160701001-Thread-PThread
+//
+//  Created by Rainer on 16/7/1.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
